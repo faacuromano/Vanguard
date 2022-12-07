f7df2cc7834ac340a96b235d197919f38ff86150
FacuRomano <facundo-romano@hotmail.com>
2022-12-07 04:54:34 -0300

    getOne slicing & half of card

---
+++ src/Components/GetOne.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './GetOne.css'
+
+const GetOne = () => {
+  return (
+    <div className='asd'>
+
+    <div className='textBloq'>
+    <h1 className='titleGetOne'>THE VANGUARD COLLECTION </h1>
+    <h5 className='descriptionGetOne'>If you want one of this artworks press in the card after to pick one of the buy options </h5>
+    </div>
+    
+    <div className='containerCard'> 
+      <div className='card'>    
+      <div className='imageCard'>
+      IMAGENIMAGEN 
+      </div>
+      <div className='descriptionCard'>    
+       DESCRIPTION
+      </div>
+     </div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default GetOne
---
+++ src/Components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Fragment } from 'react'
+import { Outlet, Link } from "react-router-dom";
+
+
+import './Homes.css'
+
+const Home = () => {
+  return (
+    <Fragment>
+
+    <div className='upBannerHome'> 
+    <h1 className='titleBanner'>{` THE VANGUARD \n TREASURE `} </h1>   
+
+    </div>
+
+    <div 
+    className='downBannerHome'
+    >
+            <Link className='buttonGetOne' to="/getOne">GET ONE</Link>
+        {/* <button className='buttonGetOne'>GET ONE</button> */}
+    </div>
+
+    </Fragment>
+  )
+}
+
+export default Home
